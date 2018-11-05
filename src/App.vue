6643cca2bc45f5b0a78c5f35ed565cfbee01fef2
siz0001 <42665078+siz0001@users.noreply.github.com>
2018-11-05 23:21:53 +0900

    작동 확인

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,80 @@
+ lines (72 sloc)  1.94 KB
 <template>
-    <v-app>
-        <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" app>
-            <v-list>
-                <v-list-tile value="true" v-for="(item, i) in items" :key="i" @click="linkto(item.linkpath)">
-                    <v-list-tile-action>
-                        <v-icon v-html="item.icon"></v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-        <v-toolbar fixed app :clipped-left="clipped">
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-btn icon @click.stop="miniVariant = !miniVariant">
-                <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-            </v-btn>
-            <v-btn icon @click.stop="clipped = !clipped">
-                <v-icon>web</v-icon>
-            </v-btn>
-            <v-btn icon @click.stop="fixed = !fixed">
-                <v-icon>remove</v-icon>
-            </v-btn>
-            <v-toolbar-title v-text="title"></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-                <v-icon>menu</v-icon>
-            </v-btn>
-        </v-toolbar>
-        <v-content>
-            <router-view></router-view>
-        </v-content>
-        <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed>
-            <v-list>
-                <v-list-tile @click.native="right = !right">
-                    <v-list-tile-action>
-                        <v-icon>compare_arrows</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-        <v-footer :fixed="fixed" app>
-            <span>&copy; 2017</span>
-        </v-footer>
-    </v-app>
-</template>
-<script>
-export default {
-    data() {
-        return {
-            clipped: false,
-            drawer: true,
-            fixed: false,
-            items: [
-                { icon: 'home', title: 'Home', linkpath: '/' },
-                { icon: 'star', title: 'Hello Vue', linkpath: '/helloworld' },
-            ],
-            miniVariant: false,
-            right: true,
-            rightDrawer: false,
-            title: 'Vuetify.js'
-        }
-    },
-    methods: {
-        linkto(pathname) {
-            this.$router.push({ path: pathname })
-        },
-    }
-}
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+                        <router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    
+      <v-tabs
+      fixed-tabs
+    centered    
+    dark
+    icons-and-text
+  >
+    
+<v-tabs-slider color="yellow"></v-tabs-slider>
+    <v-tab href="#tab-1">
+      Recents
+      <v-icon>phone</v-icon>
+    </v-tab>
 
+    <v-tab href="#tab-2">
+      Favorites
+      <v-icon>favorite</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-3">
+      Nearby
+      <v-icon>account_box</v-icon>
+    </v-tab>
+
+  </v-tabs>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
 </script>
